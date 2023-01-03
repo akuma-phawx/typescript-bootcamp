8edcd2b6097e7d3291bc4c99892af56f61a2a036
Christoforos Vradis <chrisvradis00@gmail.com>
2023-01-03 23:11:38 +0200

    chore(object): create object type

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-function add(
-  n1: number,
-  n2: number,
-  showResult: boolean,
-  resultPhrase: string
-) {
-  if (showResult) {
-    console.log(resultPhrase, n1 + n2);
-  } else {
-    return n1 + n2;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Christopher',
+  age: 28,
+};
 
-const number1 = 5;
-const number2 = 8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
