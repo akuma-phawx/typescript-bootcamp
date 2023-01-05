db6e4bfbb02e628d26434fffe9dbbb0e894abec6
Christoforos Vradis <chrisvradis00@gmail.com>
2023-01-05 23:38:08 +0200

    chore(tuples): working with tuples

--- app.ts
+++ app.ts
@@ -1,6 +1,24 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Christopher',
   age: 28,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
 
+person.role.push('admin');
+// person.role[1] = 10;
+
+let favouriteActivities: string[] = [];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
+
+// Tuples
