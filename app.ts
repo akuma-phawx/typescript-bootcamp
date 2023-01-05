33a86ea1acd71d4997218f3da63c1d311189960a
Christoforos Vradis <chrisvradis00@gmail.com>
2023-01-05 23:48:51 +0200

    chore(enums): working with enums

--- app.ts
+++ app.ts
@@ -1,24 +1,33 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Christopher',
+//   age: 28,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+// person.role.push('admin');
+// // person.role[1] = 10;
+// person.role = [2, 'S'];
+
+// ENUM
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: 'Christopher',
   age: 28,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
-
-person.role.push('admin');
-// person.role[1] = 10;
-
-let favouriteActivities: string[] = [];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
-}
-
-// Tuples
