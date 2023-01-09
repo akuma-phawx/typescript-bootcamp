e8d411a4ff6260755d357e3b203939fe7f6a801f
Christoforos Vradis <chrisvradis00@gmail.com>
2023-01-09 23:25:00 +0200

    chore(class): add class functions

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 class Department {
-  name: string;
+  //private readonly id: number
+  private employees: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private readonly id: number, private name: string) {}
+
+  describe(this: Department) {
+    console.log(`Department is ${this.name} with id ${this.id}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
-console.log(accounting);
+const accounting = new Department(1, 'Accounting');
+
+accounting.describe();
+accounting.addEmployee('Jonas');
+accounting.addEmployee('Sarah');
+accounting.printEmployeeInformation();
